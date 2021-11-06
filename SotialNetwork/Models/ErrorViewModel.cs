85818c610cd20ab8af4981cf7c2a5e49f87b123f
Dmitry Ananyev <dima-a7@hotmail.com>
2021-11-07 00:21:31 +0300

    Начальный пуш

---
+++ SotialNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SotialNetwork.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
